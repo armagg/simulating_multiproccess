96c76fa619cb7307bd540ed473c5b3a04789f63a
MohammadJavad Valizadeh <86831803+mjvz@users.noreply.github.com>
2021-07-03 11:11:47 +0430

    Update prepare.py
    other half of initial conditions

--- src/prepare.py
+++ src/prepare.py
@@ -2,8 +2,9 @@ import numpy as np
 
 def prepare_first_conditions():
             
-    x = np.arange(-9,-6.5,0.5)
-    y = np.append(x,np.arange(6.5,9,0.5))
+    y = np.arange(-9,-6.5,0.5)
+    y = np.append(y,np.arange(6.5,9,0.5))
+    x = np.arange(6.5,9,0.5)
     z = np.arange(20,25,0.5)
     args = []
     for x0 in x:
